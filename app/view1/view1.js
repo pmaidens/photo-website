2846879e184c0af23a6da1a4102e136c65e49a77
Maidens <pmaidens@EDML11b17ddb9.local>
2014-12-19 15:36:02 -0700

    Added a view for a big picture

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,5 +10,18 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', [function() {
+    this.server = "Pictures/";
+    this.image = "3fbdef1f0ce4b5b6803c2b81328c764f-xxlarge.jpg";
+    this.imageProperties = {
+        "title": "Weston Sitting",
+        "caption": "It's a good thing we got a picture of him sitting still, because it is the only time you will ever see him like this"
+    },
+    this.fullResVisible = false;
 
+    this.showFullRes = function showFullRes() {
+        this.fullResVisible = true;
+    },
+    this.hideFullRes = function showFullRes() {
+        this.fullResVisible = false;
+    }
 }]);
