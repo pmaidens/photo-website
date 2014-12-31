d9d3ad36aeb1706235eb15a1dafeb7cabc84cfc1
Peter Maidens <Peter_Maidens@intuit.com>
2014-12-31 12:21:37 -0700

    Adding grunt and compass

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
--- README.md
+++ README.md
@@ -3,6 +3,11 @@ photo-website
 
 A website that will allow any visitor to upload and download photos
 
+Requirements
+============
+
+NPM, Ruby, Compass
+
 Personal Notes:
 
 Following [this](http://cwbuecheler.com/web/tutorials/2013/node-express-mongo/) and [this](http://scotch.io/tutorials/javascript/creating-a-single-page-todo-app-with-node-and-angular) website for node information.
