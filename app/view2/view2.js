f7ef6589905746560cfaa437854b239aa9d7c592
Maidens <pmaidens@EDML11b17ddb9.local>
2014-12-30 17:13:44 -0700

    Choose File working

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,13 +9,29 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
-    this.submitPicture = function submitPicture(pictureFile) {
-        console.log(this.pictureFile);
+.controller('View2Ctrl', ['$scope', '$http', '$timeout', '$compile', '$upload', function($scope, $http, $timeout, $compile, $upload) {
+    $scope.fileReaderSupported = window.FileReader != null && (window.FileAPI == null || FileAPI.html5 != false);
 
+    $scope.submitPicture = function(pictureFile) {
+        pictureFile.title = $scope.pictureTitle;
+        pictureFile.caption = $scope.pictureCaption;
+        pictureFile.credits = $scope.pictureCredits;
+        console.log(pictureFile);
     }
 
-    this.generateThumb = function generateThumb(picture, files) {
-        console.log(picture);
+    $scope.generateThumb = function(file) {
+        if (file != null) {
+            if ($scope.fileReaderSupported && file.type.indexOf('image') > -1) {
+                $timeout(function() {
+                    var fileReader = new FileReader();
+                    fileReader.readAsDataURL(file);
+                    fileReader.onload = function(e) {
+                        $timeout(function() {
+                            file.dataUrl = e.target.result;
+                        });
+                    }
+                });
+            }
+        }
     }
 }]);
